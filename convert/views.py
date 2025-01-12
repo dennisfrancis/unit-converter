901208a19a9673ed99116ca88c237f336217dc21
Dennis Francis <dennisfrancis.in@gmail.com>
2025-01-12 17:01:25 +0530

    barebones of all three views with templates
    Signed-off-by: Dennis Francis <dennisfrancis.in@gmail.com>

--- convert/views.py
+++ convert/views.py
@@ -4,9 +4,17 @@ from django.views import View
 
 class LengthView(View):
     template_name = "convert/length.html"
+    convert_type = "length"
+    units = ['millimeter', 'centimeter', 'meter',
+             'kilometer', 'inch', 'foot', 'yard', 'mile']
 
     def get(self, request):
-        return render(request, self.template_name)
+        ctx = {"units": self.units,
+               "convert_type": self.convert_type,
+               }
+        return render(request,
+                      self.template_name,
+                      ctx)
 
     def post(self, request):
         return render(request, self.template_name)
@@ -14,9 +22,16 @@ class LengthView(View):
 
 class WeightView(View):
     template_name = "convert/weight.html"
+    convert_type = "weight"
+    units = ['milligram', 'gram', 'kilogram', 'ounce', 'pound']
 
     def get(self, request):
-        return render(request, self.template_name)
+        ctx = {"units": self.units,
+               "convert_type": self.convert_type,
+               }
+        return render(request,
+                      self.template_name,
+                      ctx)
 
     def post(self, request):
         return render(request, self.template_name)
@@ -24,6 +39,23 @@ class WeightView(View):
 
 class TemperatureView(View):
     template_name = "convert/temperature.html"
+    convert_type = "temperature"
+    units = ['celsius', 'fahrenheit', 'kelvin']
+
+    def get(self, request):
+        ctx = {"units": self.units,
+               "convert_type": self.convert_type,
+               }
+        return render(request,
+                      self.template_name,
+                      ctx)
+
+    def post(self, request):
+        return render(request, self.template_name)
+
+
+class ResultView(View):
+    template_name = "convert/result.html"
 
     def get(self, request):
         return render(request, self.template_name)
