942aa1a281d741f5dbfbf9a6e7bfd9ca0c1e070d
Dennis Francis <dennisfrancis.in@gmail.com>
2025-01-14 15:57:40 +0530

    feat: /convert defaults to /convert/length behavior without redirect
    Signed-off-by: Dennis Francis <dennisfrancis.in@gmail.com>

--- convert/urls.py
+++ convert/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'convert'
 urlpatterns = [
+    path('', views.LengthView.as_view(), name='root'),
     path('length', views.LengthView.as_view(), name='length'),
     path('weight', views.WeightView.as_view(), name='weight'),
     path('temperature', views.TemperatureView.as_view(), name='temperature'),
